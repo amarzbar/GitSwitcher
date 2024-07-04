5e2b1f7e14e715c1461a11bd19680109a8476913
Marz <58681233+amarzbar@users.noreply.github.com>
2024-07-03 20:42:05 -0400

    Update Switcher.sh

--- Switcher.sh
+++ Switcher.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
+# Clear the environment
 env -i
-git config --local user.name Ammar Hussein
-git config --local user.email InsertEmail@domain.com
-git config --local http.sslverify false
+
+# Function to set git configuration for work
+set_work_config() {
+    git config --local user.name "YOUR NAME"
+    git config --local user.email "COMPANY-EMAIL"
+    git config --local http.sslverify false
+    echo "Local git configuration set to Work."
+}
+
+# Function to set git configuration for personal
+set_personal_config() {
+    git config --local user.name "GitHub DisplayName"
+    git config --local user.email "PERSONAL EMAIL"
+    git config --local http.sslverify false
+    echo "Local git configuration set to Personal."
+}
+
+# Check the argument provided
+case "$1" in
+    --work|-w)
+        set_work_config
+        ;;
+    --personal|-p)
+        set_personal_config
+        ;;
+    *)
+        echo "Usage: $0 [--work|-w|--personal|-p]"
+        exit 1
+        ;;
+esac
